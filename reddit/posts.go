b477bf8adc0098f7677544fc63644c31bdae1358
Andrew Arrow <oneone@gmail.com>
2024-07-09 09:16:30 -0700

    fixed SQL injection vulnerability

--- reddit/posts.go
+++ reddit/posts.go
@@ -1,21 +1,79 @@
 package reddit
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/andrewarrow/feedback/models"
 )
 
 func GetPosts(sub string) []*models.BaseModel {
-	js := doRedditGet()
-	fmt.Println(len(js))
+	//htmlString := doRedditGet()
+	htmlString, _ := os.ReadFile("/Users/aa/Documents/guns.txt")
+
+	doc, _ := goquery.NewDocumentFromReader(bytes.NewReader(htmlString))
+
+	processDocument(doc)
+
 	return nil
 }
 
+func processDocument(doc *goquery.Document) {
+	doc.Find("div").Each(func(i int, s *goquery.Selection) {
+		processDiv(s)
+	})
+}
+
+func processDiv(s *goquery.Selection) {
+	s.Find("p.title").Each(func(i int, p *goquery.Selection) {
+		processTitleP(p)
+	})
+}
+
+func processTitleP(p *goquery.Selection) {
+	p.Find("a").Each(func(i int, a *goquery.Selection) {
+		printLink(a)
+	})
+}
+
+func printLink(a *goquery.Selection) {
+	linkText := a.Text()
+	href, exists := a.Attr("href")
+	if exists {
+		fmt.Printf("Link text: %s\n", linkText)
+		if strings.HasPrefix(href, "/r/GunsNRoses/comments") {
+			fmt.Printf("Href: %s\n", href)
+		}
+	}
+}
+
 func doRedditGet() string {
+	//"github.com/PuerkitoBio/goquery"
+	url := "https://old.reddit.com/r/gunsnroses/new"
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:127.0) Gecko/20100101 Firefox/127.0")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return ""
+	}
+	b, _ := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Error response from server:", resp.Status)
+		return string(b)
+	}
+
+	return string(b)
+}
+func doApiRedditGet() string {
 	apiKey := os.Getenv("REDDIT_API")
 	url := "https://api.reddit.com/r/gunsnroses/new"
 
