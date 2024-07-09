9e3363bec5ebd7722713c830449ca9c4b75cdd44
Andrew Arrow <oneone@gmail.com>
2024-07-09 09:28:33 -0700

    fixed broken link

--- app/seed.go
+++ app/seed.go
@@ -8,11 +8,11 @@ import (
 
 func Seed(c *router.Context) {
 	posts := reddit.GetPosts("gunsnroses")
-	c.Params = map[string]any{}
 	for _, post := range posts {
+		c.Params = map[string]any{}
 		c.Params["title"] = post.GetString("title")
 		c.Params["url"] = post.GetString("url")
 		c.Params["id_reddit"] = post.GetString("id_reddit")
+		c.ValidateAndInsert("post")
 	}
-	c.ValidateAndInsert("post")
 }
