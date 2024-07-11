5fc8e6665c29c77f170ae577179a8d895628eeed
Andrew Arrow <oneone@gmail.com>
2024-07-11 05:36:39 -0700

    updated API documentation

--- headless.py
+++ headless.py
@@ -1,23 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
 import sys
-import random
-import time
-import os
 import json
 from bs4 import BeautifulSoup
 from parsel import Selector
 
-
-def run(route, filename):
+def fetch_page(route):
+    print(route)
     options = Options()
-    #options.headless = True
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_argument("--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:127.0) Gecko/20100101 Firefox/127.0")
     options.add_argument("--no-sandbox")
@@ -25,48 +18,59 @@ def run(route, filename):
     options.add_argument("--disable-infobars")
     options.add_argument("--disable-extensions")
     options.add_argument('-headless')
+    
     browser = webdriver.Chrome(options=options)
-    browser.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument', {
-    'source': '''
-        Object.defineProperty(navigator, 'webdriver', {
-            get: () => undefined
-        })
-    '''
-    })
-
 
     browser.get(route)
     wait = WebDriverWait(browser, 10)
     wait.until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
 
     rendered_html = browser.page_source
-    soup = BeautifulSoup(rendered_html, 'html.parser')
+    browser.quit()
+    
+    return rendered_html
+
+def parse_page(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
 
     for script in soup.find_all('script'):
         script.extract()
 
     soup = BeautifulSoup(str(soup), 'html.parser')
-
-    html_content = soup.prettify()
-    selector = Selector(text=html_content)
+    selector = Selector(text=soup.prettify())
 
     titles = selector.css('p.title')
-
     results = []
 
     for title in titles:
         href = title.css('a::attr(href)').get()
         text = title.css('a::text').get()
-
         results.append({ "Link": href, "Text": text })
 
-    with open(f"titles_{filename}.json" , 'w') as f:
-      json.dump(results, f, indent=4)
-
     next_button_href = selector.css('span.next-button a::attr(href)').get()
-    print(next_button_href)
+    
+    return results, next_button_href
 
-if len(sys.argv) > 2:
-  route = sys.argv[1]
-  title = sys.argv[2]
-  run(route, title)
+def run(start_route, filename):
+    route = start_route
+    count = 1
+    all_results = []
+
+    while route:
+        html_content = fetch_page(route)
+        results, next_button_href = parse_page(html_content)
+
+        with open(f"data/{count}.json", 'w') as f:
+            json.dump(results, f, indent=4)
+
+        count += 1
+        route = next_button_href
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        start_route = sys.argv[1]
+        filename = "results"
+        run(start_route, filename)
+    else:
+        print("Please provide the initial route as a command-line argument.")
+
