9920d3e447500875648f3dcac52b952e2a92153e
Andrew Arrow <oneone@gmail.com>
2024-07-10 18:53:59 -0700

    fixed memory allocation issue

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/andrewarrow/feedback/router"
@@ -42,36 +43,42 @@ func main() {
 	} else if arg == "render" {
 		router.RenderMarkup()
 	} else if arg == "balancer" {
+
+	} else if arg == "run" {
 		handler := http.HandlerFunc(handleRequest)
 		s := &http.Server{
-			Addr:    ":443",
+			Addr:    ":8080",
 			Handler: handler,
 		}
 
-		s.ListenAndServe()
+		go s.ListenAndServe()
 
-	} else if arg == "run" {
 		router.BuildTag = buildTag
 		router.EmbeddedTemplates = embeddedTemplates
 		router.EmbeddedAssets = embeddedAssets
 		r := router.NewRouter("DATABASE_URL", embeddedFile)
 		r.Paths["/"] = app.Welcome
 		r.Paths["core"] = app.Core
-		r.Paths["kibana"] = app.Kibana
 		//r.Paths["api"] = app.HandleApi
 		//r.Paths["login"] = app.Login
 		//r.Paths["register"] = app.Register
 		//r.Paths["admin"] = app.Admin
 		r.Paths["markup"] = router.Markup
 		r.BucketPath = "/Users/aa/bucket"
-		r.ListenAndServe(":" + os.Args[2])
+		r.ListenAndServe(":" + "3001")
 	} else if arg == "help" {
 	}
 }
 
 func handleRequest(writer http.ResponseWriter, request *http.Request) {
-	query := c.Request.URL.RawQuery
-	target := "http://127.0.0.1:5601"
+	path := request.URL.Path
+	target := "http://127.0.0.1:3001"
+
+	if strings.Contains(path, "kibana") {
+		target = "http://127.0.0.1:5601"
+	}
+
+	query := request.URL.RawQuery
 	if query != "" {
 		target += "?" + query
 	}
@@ -79,6 +86,6 @@ func handleRequest(writer http.ResponseWriter, request *http.Request) {
 	fmt.Println(err)
 
 	proxy := httputil.NewSingleHostReverseProxy(proxyURL)
-	c.Request.Host = proxyURL.Host
+	request.Host = proxyURL.Host
 	proxy.ServeHTTP(writer, request)
 }
